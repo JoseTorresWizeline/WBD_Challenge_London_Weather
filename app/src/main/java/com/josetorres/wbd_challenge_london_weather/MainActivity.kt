3ac3418c59977f1496e06c7c29cb1e5c7c2ae5d9
JoseTorres <jose.torres@wizeline.com>
2022-10-17 09:57:15 -0500

    Initial commit

---
+++ app/src/main/java/com/josetorres/wbd_challenge_london_weather/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.josetorres.wbd_challenge_london_weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
