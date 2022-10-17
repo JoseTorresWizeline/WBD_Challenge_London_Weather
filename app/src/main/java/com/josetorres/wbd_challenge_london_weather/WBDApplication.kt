8820072aecadf685b79b05e0f6460cb06f203432
JoseTorres <jose.torres@wizeline.com>
2022-10-17 13:26:12 -0500

    feat: Created the application file and added the internet permission in the manifest

---
+++ app/src/main/java/com/josetorres/wbd_challenge_london_weather/WBDApplication.kt
@@ -0,0 +1,8 @@
+package com.josetorres.wbd_challenge_london_weather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WBDApplication : Application(){
+}
